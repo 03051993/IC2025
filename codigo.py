a321f671f9ddae0fa8a7cd015e7bd15446ab86af
03051993 <lidiane.souza@discentes.fat.uerj.br>
2025-03-24 11:02:58 -0300

    Add files via upload

---
+++ codigo.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+# Título da aplicação
+st.title("Inversor de Frases")
+
+# Entrada de texto
+frase = st.text_input("Digite uma frase:")
+
+# Botão para inverter a frase
+if st.button("Inverter frase"):
+    # Verifica se a frase foi preenchida
+    if frase:
+        # Exibe a frase invertida
+        frase_invertida = frase[::-1]
+        st.write("Frase invertida:")
+        st.write(frase_invertida)
+    else:
+        st.write("Por favor, digite uma frase para inverter.")
+
